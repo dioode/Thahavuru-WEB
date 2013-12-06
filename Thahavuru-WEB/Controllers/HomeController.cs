e2fc1e803ebfeb48e03bee15ea5f343999a600bb
jayendra_ka <jayendra.k.a@gmail.com>
2013-12-07 05:03:57 +0530

    home contraller

--- Thahavuru-WEB/Controllers/HomeController.cs
+++ Thahavuru-WEB/Controllers/HomeController.cs
@@ -16,16 +16,23 @@ namespace Test_Web.Controllers
 {
     public class HomeController : Controller
     {
-        //
-        // GET: /Home/
+        #region Action Methods
 
         public ActionResult Index()
         {
-            
             return View();
         }
 
+        #endregion
+
+        #region Partial View Action
+
         public ActionResult Header()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Footer()
         {
             return PartialView();
         }
@@ -47,11 +54,6 @@ namespace Test_Web.Controllers
             return PartialView(null);
         }
 
-        public ActionResult Footer()
-        {
-            return PartialView();
-        }
-
         public PartialViewResult ResultImages()
         {
             SessionDataManager SM = new SessionDataManager();
@@ -64,7 +66,19 @@ namespace Test_Web.Controllers
             return PartialView(null);
         }
 
-        public JsonResult ClearResults(object data) 
+        public ActionResult Upload(HttpPostedFileBase FileData, FormCollection forms)
+        {
+            var file = Request.Files["Filedata"];
+            string savePath = Server.MapPath(@"~\Content\" + file.FileName);
+            file.SaveAs(savePath);
+            return Content(Url.Content(@"~\Content\" + file.FileName));
+        }
+
+        #endregion
+
+        #region Json Requests
+
+        public JsonResult ClearResults(object data)
         {
             SessionDataManager SM = new SessionDataManager();
             SM.Clear(SessionDataManager.Key.pageImageDataModel);
@@ -88,7 +102,7 @@ namespace Test_Web.Controllers
 
             if (UIM == null || pageData == null) return Json(false, JsonRequestBehavior.AllowGet);
 
-            if (data == "Next") 
+            if (data == "Next")
             {
                 int nextPage = pageData.PageNumber + 1;
                 if (nextPage <= UIM.MaxLeaves)
@@ -110,71 +124,6 @@ namespace Test_Web.Controllers
             return Json(true, JsonRequestBehavior.AllowGet);
         }
 
-        private static void ConvertModel(SessionDataManager SM, UserInterfaceModel UIM, PageImageDataModel pageData, int page)
-        {
-            pageData.ImageList = new List<ImageModel>();
-            if (UIM.SearchingPerson.MatchedFaceIdSet[page] != null)
-            {
-                for (int i = 0; i < UIM.SearchingPerson.MatchedFaceIdSet[page].Count; i++)
-                {
-                    ImageModel img = new ImageModel();
-                    img.MatchedImageURI = @"http://localhost/imgs/User%20(" + UIM.SearchingPerson.MatchedFaceIdSet[page][i] + ").jpg";
-                    img.UserName = "User (" + UIM.SearchingPerson.MatchedFaceIdSet[page][i]+")";
-                    img.Address = "Address (" + UIM.SearchingPerson.MatchedFaceIdSet[page][i]+")";
-                    pageData.ImageList.Add(img);
-                }
-            }
-
-            if (pageData.AdvancedSearchUserName != null && pageData.AdvancedSearchUserName.Trim() != "")
-            {
-                DataAccessSingleton dAS = new DataAccessSingleton();
-                List<PersonVM> persons = dAS.GetPersonByName(pageData.AdvancedSearchUserName);
-                if (persons != null && persons.Count != 0) 
-                {
-                    PersonVM person = persons.First();
-                    ImageModel img = new ImageModel();
-                    img.MatchedImageURI = @"http://localhost/imgs/User%20(" + person.Id+ ").jpg";
-                    img.UserName = person.Name;
-                    img.Address = person.Address;
-                    pageData.AdvancedSearchResult = img;
-                }
-                else
-                {
-                    pageData.AdvancedSearchResult = null;
-                }
-            }
-            else
-            {
-                pageData.AdvancedSearchResult = null;
-            }
-
-            pageData.attributeValues = new List<KeyValuePair<string, string>>();
-            foreach (var item in UIM.SearchingPerson.SearchTrakKeeper[UIM.PageNumber-1])
-            {
-                var atribute = UIM.SearchingPerson.FaceofP.FaceAttributes[item[0]-1];
-                pageData.attributeValues.Add(new KeyValuePair<string, string>(atribute.Name, atribute.ClassesInOrder.Where(x =>x.ClassNumber == item[1]).First().Name));
-            }
-
-            pageData.PageNumber = page;
-            pageData.Next = pageData.PageNumber == (UIM.MaxLeaves) ? false : true;
-            pageData.Back = pageData.PageNumber == 1 ? false : true;
-
-            SM.Clear(SessionDataManager.Key.pageImageDataModel);
-            SM.Clear(SessionDataManager.Key.userInterfaceModel);
-
-            SM.Set(SessionDataManager.Key.pageImageDataModel, pageData);
-            SM.Set(SessionDataManager.Key.userInterfaceModel, UIM);
-        }
-
-        public ActionResult Upload(HttpPostedFileBase FileData, FormCollection forms)
-        {
-            var file = Request.Files["Filedata"];
-            string savePath = Server.MapPath(@"~\Content\" + file.FileName);
-            file.SaveAs(savePath);
-            return Content(Url.Content(@"~\Content\" + file.FileName));
-        }
-
-        //[AsyncTimeout(30000)]
         public JsonResult Recognize(string imageName, string userName)
         {
             string savePath = Server.MapPath(imageName);
@@ -193,5 +142,68 @@ namespace Test_Web.Controllers
 
             return Json(true, JsonRequestBehavior.AllowGet);
         }
+        
+        #endregion
+
+        #region Private Methods
+
+        private static void ConvertModel(SessionDataManager SM, UserInterfaceModel UIM, PageImageDataModel pageData, int page)
+        {
+            pageData.ImageList = new List<ImageModel>();
+            if (UIM.SearchingPerson.MatchedFaceIdSet[page] != null)
+            {
+                for (int i = 0; i < UIM.SearchingPerson.MatchedFaceIdSet[page].Count; i++)
+                {
+                    ImageModel img = new ImageModel();
+                    img.MatchedImageURI = @"http://localhost/imgs/User%20(" + UIM.SearchingPerson.MatchedFaceIdSet[page][i] + ").jpg";
+                    img.UserName = "User (" + UIM.SearchingPerson.MatchedFaceIdSet[page][i] + ")";
+                    img.Address = "Address (" + UIM.SearchingPerson.MatchedFaceIdSet[page][i] + ")";
+                    pageData.ImageList.Add(img);
+                }
+            }
+
+            if (pageData.AdvancedSearchUserName != null && pageData.AdvancedSearchUserName.Trim() != "")
+            {
+                DataAccessSingleton dAS = new DataAccessSingleton();
+                List<PersonVM> persons = dAS.GetPersonByName(pageData.AdvancedSearchUserName);
+                if (persons != null && persons.Count != 0)
+                {
+                    PersonVM person = persons.First();
+                    ImageModel img = new ImageModel();
+                    img.MatchedImageURI = @"http://localhost/imgs/User%20(" + person.Id + ").jpg";
+                    img.UserName = person.Name;
+                    img.Address = person.Address;
+                    pageData.AdvancedSearchResult = img;
+                }
+                else
+                {
+                    pageData.AdvancedSearchResult = null;
+                }
+            }
+            else
+            {
+                pageData.AdvancedSearchResult = null;
+            }
+
+            pageData.attributeValues = new List<KeyValuePair<string, string>>();
+            foreach (var item in UIM.SearchingPerson.SearchTrakKeeper[UIM.PageNumber - 1])
+            {
+                var atribute = UIM.SearchingPerson.FaceofP.FaceAttributes[item[0] - 1];
+                pageData.attributeValues.Add(new KeyValuePair<string, string>(atribute.Name, atribute.ClassesInOrder.Where(x => x.ClassNumber == item[1]).First().Name));
+            }
+
+            pageData.PageNumber = page;
+            pageData.Next = pageData.PageNumber == (UIM.MaxLeaves) ? false : true;
+            pageData.Back = pageData.PageNumber == 1 ? false : true;
+
+            SM.Clear(SessionDataManager.Key.pageImageDataModel);
+            SM.Clear(SessionDataManager.Key.userInterfaceModel);
+
+            SM.Set(SessionDataManager.Key.pageImageDataModel, pageData);
+            SM.Set(SessionDataManager.Key.userInterfaceModel, UIM);
+        }
+
+        #endregion
+
     }
 }
