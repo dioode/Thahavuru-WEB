e2804162abc23ea24eee695a7379b05bf03abcf8
sahan_jayawardana <sahan.jayawardana@gmail.com>
2013-12-06 12:33:00 +0530

    [added database model]

---
+++ Thahavuru-WEB/Models/IndClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Thahavuru_WEB.Models
+{
+    [DataContract]
+    public class IndClass
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public int ClassNumber { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
