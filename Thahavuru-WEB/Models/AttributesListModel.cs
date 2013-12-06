522d5839560639e59150db5df1524c33ebc8df99
sahan_jayawardana <sahan.jayawardana@gmail.com>
2013-12-06 12:30:13 +0530

    [added classes for classification attributes]

---
+++ Thahavuru-WEB/Models/AttributesListModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace Thahavuru_WEB.Models
+{
+    [DataContract]
+    public class AttributesListModel
+    {
+        [DataMember]
+        public List<AttributeModel> AttributeList { get; set; }
+    }
+}
